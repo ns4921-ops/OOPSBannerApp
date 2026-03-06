eecd96aced1571927bae46fec16313ae9dcaee7e
ns4921-op <ns4921@srmist.edu.in>
2026-03-06 23:39:51 +0530

    add program to print ascii oops

---
+++ .src/UC2_OOPSBannerApp.java
@@ -0,0 +1,18 @@
+public class UC2_OOPSBannerApp {
+    // Main method to run the banner display
+    // Print each line of the OOPS banner to create the visual effect for the
+    // message "OOPS". Construct each line using string concatenation.
+    // Each line corresponds to a row in the banner for the letters O, O, P, S.
+    // Using individual print statements for each line.
+    public static void main(String[] args) {
+
+        // UC2 Simple ASCII Banner
+        System.out.println("  *****   *****   ********  ******  ");
+        System.out.println(" *     * *     *  *      *  *       ");
+        System.out.println("*       *       * *      *  *       ");
+        System.out.println("*       *       * ********  *****   ");
+        System.out.println("*       *       * *              *  ");
+        System.out.println(" *     * *     *  *        *     *  ");
+        System.out.println("  *****   *****   *         *****   ");
+    }
+}
